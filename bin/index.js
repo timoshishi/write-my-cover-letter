4d80d143856750963092c5957026f3a25014e21e
timoshishi <timfrrst@gmail.com>
2021-03-06 11:57:19 -0500

    Write to PDF

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ const initCommander = require('../lib/initCommander.js');
 const { generateParagraphs } = require('../lib/letterBody.js');
 const writeTxt = require('../lib/writeTxt.js');
 const writeDocx = require('../lib/writeDocx');
+const writePDF = require('../lib/writePDF.js');
 
 const options = initCommander();
 const paras = generateParagraphs(options);
@@ -12,6 +13,16 @@ if (options.debug) console.log(options);
 
 if (options.txt && !options.docx) {
   writeTxt(paras);
-} else {
-  writeDocx(paras);
+  console.log('txt written');
+} else if (options.pdf) {
+  writeDocx(paras)
+    .then((un) => {
+      writePDF(options);
+      console.log('pdf written');
+    })
+    .catch((err) => console.error(err));
+} else if (options.docx) {
+  writeDocx(paras)
+    .then((un) => console.log('docx written'))
+    .catch((err) => console.error(err));
 }
