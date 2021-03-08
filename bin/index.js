d67caa77e31eca44c99d69b6629e6688c16b79cb
timoshishi <timfrrst@gmail.com>
2021-03-08 11:54:35 -0500

    Refactor to use Inquirer.js

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
 
-const initCommander = require('../lib/initCommander.js');
+// const initCommander = require('../lib/initCommander.js');
 const { generateParagraphs } = require('../lib/letterBody.js');
-const writeTxt = require('../lib/writeTxt.js');
-const writeDocx = require('../lib/writeDocx');
-const writePDF = require('../lib/writePDF.js');
+// const writeTxt = require('../lib/writeTxt.js');
+// const writeDocx = require('../lib/writeDocs/writeDocx');
+// const writePDF = require('../lib/writeDocs/writePDF.js');
+const initInquirer = require('../lib/initInquirer.js');
 
-const options = initCommander();
-const paras = generateParagraphs(options);
+// const options = initCommander();
+initInquirer();
+// const paras = generateParagraphs(options);
 
-if (options.debug) console.log(options);
+// if (options.debug) console.log(options);
 
-if (options.txt && !options.docx) {
-  writeTxt(paras);
-  console.log('txt written');
-} else if (options.pdf) {
-  writeDocx(paras)
-    .then((un) => {
-      writePDF(options);
-      console.log('pdf written');
-    })
-    .catch((err) => console.error(err));
-} else if (options.docx) {
-  writeDocx(paras)
-    .then((un) => console.log('docx written'))
-    .catch((err) => console.error(err));
-}
+// if (options.txt && !options.docx) {
+//   writeTxt(paras);
+//   console.log('txt written');
+// } else if (options.pdf) {
+//   writeDocx(paras)
+//     .then((un) => {
+//       writePDF(options);
+//       console.log('pdf written');
+//     })
+//     .catch((err) => console.error(err));
+// } else if (options.docx) {
+//   writeDocx(paras)
+//     .then((un) => console.log('docx written'))
+//     .catch((err) => console.error(err));
+// }
