aac344218ee29229d42e74ab125f05a3cd6f0a7c
timoshishi <timfrrst@gmail.com>
2021-03-09 19:29:15 -0500

    Implement writeDocx.test

--- __tests__/writeDocx.test.js
+++ __tests__/writeDocx.test.js
@@ -13,25 +13,29 @@ const options = {
 };
 const paras = generateParagraphs(options);
 
-const fileName = `${paras.name.split(' ').join('_')}.docx`;
+const fileName = `${paras.name.split(' ').join('_')}_cover_letter.docx`;
 
-afterEach(() => {
-  fs.unlinkSync(path.resolve('..', __dirname, `${fileName}`));
-});
+// afterEach(() => {
+//   fs.unlinkSync(path.resolve(__dirname, `${fileName}`));
+// });
 
 test('it should write a file to disk', () => {
-  const beforeWrite = fs.readdirSync(path.resolve('..', __dirname));
-  return writeDocx(paras).then((un) => {
-    console.log({ un });
-    const afterWrite = fs.readdirSync(path.resolve('..', __dirname));
-    expect(afterWrite.length).toBe(beforeWrite.length + 1);
-  });
+  const beforeWrite = fs.readdirSync(path.resolve(__dirname));
+  return writeDocx(paras, __dirname)
+    .then((un) => {
+      console.log({ un });
+      const afterWrite = fs.readdirSync(path.resolve(__dirname));
+      expect(afterWrite.length).toBe(beforeWrite.length + 1);
+    })
+    .then(() => fs.unlinkSync(path.resolve(__dirname, `${fileName}`)));
 });
 
 test('it should write a docx file', () => {
-  return writeDocx(paras).then((un) => {
-    const files = fs.readdirSync(path.resolve('..', __dirname));
+  return writeDocx(paras, __dirname)
+    .then((un) => {
+      const files = fs.readdirSync(path.resolve(__dirname));
 
-    expect(files).toContain(fileName);
-  });
+      expect(files).toContain(fileName);
+    })
+    .then((un) => fs.unlinkSync(path.resolve(__dirname, `${fileName}`)));
 });
