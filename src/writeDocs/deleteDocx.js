1d07581d480a7da3f296211ecb8d5d092471cf93
timoshishi <timfrrst@gmail.com>
2022-09-23 16:38:39 -0400

    init-inquirer

--- src/writeDocs/deleteDocx.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const deleteDocx = (name) => {
-  const formattedName = name.split(' ').join('_');
-  const enterPath = `${formattedName}_cover_letter.docx`;
-  fs.unlinkSync(enterPath, (err) => err && console.error(err));
-};
-module.exports = deleteDocx;
