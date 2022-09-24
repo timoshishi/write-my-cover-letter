62ac2d233e6202eeae2969c98ede5e11827b71f1
timoshishi <timfrrst@gmail.com>
2022-09-24 13:44:25 -0400

    createCoverQuestions, writeDocx tests

--- __tests__/writeDocx.test.ts
+++ __tests__/writeDocx.test.ts
@@ -1,24 +1,80 @@
 import writeDocx from '../src/writeDocs/writeDocx';
 import path from 'path';
 import fs from 'fs';
+import { personalData, cvText } from './__mocks__';
 
-const fileName = `${paras.name.split(' ').join('_')}_cover_letter.docx`;
-
-afterEach(() => {
-  fs.unlinkSync(path.resolve(__dirname, `${fileName}`));
-});
-
-test('it should write a file to disk', () => {
-  const beforeWrite = fs.readdirSync(path.resolve(__dirname));
-  return writeDocx(paras as any, __dirname).then((un) => {
-    const afterWrite = fs.readdirSync(path.resolve(__dirname));
-    expect(afterWrite.length).toBe(beforeWrite.length + 1);
+describe('writeDocx', () => {
+  beforeEach(() => {
+    // remove all files from the __tests__/test-docs directory
+    const testDocsDir = path.resolve(__dirname, 'test-docs');
+    const files = fs.readdirSync(testDocsDir);
+    files.forEach((file) => {
+      fs.unlinkSync(path.join(testDocsDir, file));
+    });
   });
-});
+  afterEach(() => {
+    // remove all files from the __tests__/test-docs directory
+    const testDocsDir = path.resolve(__dirname, 'test-docs');
+    const files = fs.readdirSync(testDocsDir);
+    files.forEach((file) => {
+      fs.unlinkSync(path.join(testDocsDir, file));
+    });
+  });
 
-test('it should write a docx file with a properly formatted title', () => {
-  return writeDocx(paras as any, __dirname).then((un) => {
-    const files = fs.readdirSync(path.resolve(__dirname));
-    expect(files).toContain(fileName);
+  test('it should write a single file to disk if createCopy is false', async () => {
+    try {
+      const beforeWrite = fs.readdirSync(path.resolve(__dirname, 'test-docs'));
+      console.log('beforeWrite', beforeWrite);
+      await writeDocx(
+        {
+          personalData,
+          cvText,
+          createCopy: false,
+          company: 'test',
+        },
+        path.resolve(__dirname, 'test-docs')
+      );
+      const afterWrite = fs.readdirSync(path.resolve(__dirname, 'test-docs'));
+      console.log('afterWrite', afterWrite);
+      expect(afterWrite.length).toBe(beforeWrite.length + 1);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  test('it should write two files to disk if createCopy is true', async () => {
+    try {
+      const beforeWrite = fs.readdirSync(path.resolve(__dirname, 'test-docs'));
+      console.log('beforeWrite', beforeWrite);
+      await writeDocx(
+        {
+          personalData,
+          cvText,
+          createCopy: true,
+          company: 'test',
+        },
+        path.resolve(__dirname, 'test-docs')
+      );
+      const afterWrite = fs.readdirSync(path.resolve(__dirname, 'test-docs'));
+      console.log('afterWrite', afterWrite);
+      expect(afterWrite.length).toBe(beforeWrite.length + 2);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  test('it should return text that includes the cvText', async () => {
+    const text = await writeDocx(
+      {
+        personalData,
+        cvText,
+        createCopy: true,
+        company: 'test',
+      },
+      path.resolve(__dirname, 'test-docs')
+    );
+    expect(text).toContain(JSON.stringify(cvText));
+    expect(typeof text).toBe('string');
+    expect(JSON.parse(text as string)).toEqual(cvText);
   });
 });
