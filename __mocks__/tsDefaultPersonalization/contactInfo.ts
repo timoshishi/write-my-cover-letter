fdae2fbb1afda8c5cb395805b590d034ea40c094
timoshishi <timfrrst@gmail.com>
2022-09-25 13:14:21 -0400

    feat(pers): shouldUpdate and apply personalData

--- __mocks__/tsDefaultPersonalization/contactInfo.ts
+++ __mocks__/tsDefaultPersonalization/contactInfo.ts
@@ -2,5 +2,5 @@ export const contactInfo = {
   name: 'Tim Forrest',
   phone: '(734) 353-0951',
   email: 'timfrrst@gmail.com',
-  sites: ['github.com/timoshishi', 'linkedin.com/in/timfrrst'],
+  sites: [],
 };
