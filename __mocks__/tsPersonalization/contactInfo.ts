a1e545da93db6ad623292cb2206be58a0e9299fd
timoshishi <timfrrst@gmail.com>
2022-09-25 10:04:17 -0400

    feat(pers): cvPersonaliztion defaults to empty keys

--- __mocks__/tsPersonalization/contactInfo.ts
+++ __mocks__/tsPersonalization/contactInfo.ts
@@ -1,6 +1,6 @@
 export const contactInfo = {
-  name: 'Tim Forrest',
-  phone: '(734) 353-0951',
-  email: 'timfrrst@gmail.com',
-  sites: ['github.com/timoshishi', 'linkedin.com/in/timfrrst'],
+  name: '',
+  phone: '',
+  email: '',
+  sites: [],
 };
--- __tests__/readPersonalization.test.ts
+++ __tests__/readPersonalization.test.ts
@@ -2,7 +2,9 @@ import * as tsPersonalization from '../__mocks__/tsPersonalization';
 import fs from 'fs';
 import path from 'path';
 import { readPersonalization } from '../src/readPersonalization';
-const { aboutMe, contactInfo, roles } = tsPersonalization;
+import { aboutMe, contactInfo, roles } from '../__mocks__/tsPersonalization';
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
 
 describe('readPersonalization', () => {
   it('It should return one object per file in cvPersonalization', async () => {
@@ -31,5 +33,35 @@ describe('readPersonalization', () => {
       expect(Object.keys(personalData.contactInfo).length).toEqual(Object.keys(contactInfo).length);
       expect(Object.keys(personalData.roles).length).toEqual(Object.keys(roles).length);
     }
+  });
+
+  it('should read from the defaultPersonalization folder if the second parameter is set to false', async () => {
+    const personalData = await readPersonalization([__dirname, '..', '__mocks__']);
+    if (personalData) {
+      const personalDataKeys = Object.keys(personalData);
+      const tsPersonalizationKeys = Object.keys(tsPersonalization);
+      expect(personalDataKeys.length).toEqual(tsPersonalizationKeys.length);
+      expect(personalData.aboutMe.aboutMe.length).toEqual(aboutMe.length);
+      expect(personalData.contactInfo.email.length).toEqual(contactInfo.email.length);
+      expect(Object.keys(personalData.contactInfo).length).toEqual(Object.keys(contactInfo).length);
+      expect(Object.keys(personalData.roles).length).toEqual(Object.keys(roles).length);
+    }
+  });
+
+  it('should not have any length for the key in a file on first pass', async () => {
+    const personalData = await readPersonalization([__dirname, '..', '__mocks__']);
+    expect(personalData!.roles.fullstack.length).toEqual(0);
+    expect(personalData!.roles.frontend.length).toEqual(0);
+    expect(personalData!.roles.backend.length).toEqual(0);
+    expect(personalData?.contactInfo.email.length).toEqual(0);
+    expect(personalData?.contactInfo.name.length).toEqual(0);
+    expect(personalData?.contactInfo.phone.length).toEqual(0);
+    expect(personalData?.contactInfo.sites.length).toEqual(0);
+    expect(personalData?.aboutMe.aboutMe.length).toEqual(0);
+  });
+
+  it('should return undefined if there is an error', async () => {
+    const personalData = await readPersonalization([__dirname, '..', '__mocks__', 'error']);
+    expect(personalData).toBeUndefined();
   });
 });
