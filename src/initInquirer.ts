058b135f4d7ee28350a748164b881a165130e004
timoshishi <timfrrst@gmail.com>
2022-09-25 13:49:53 -0400

    feat(pers): recursive menu options

--- src/initInquirer.ts
+++ src/initInquirer.ts
@@ -2,20 +2,35 @@ import inquirer from 'inquirer';
 import { applyDefaultResponses } from './applyDefaultResponses';
 import { createCoverQuestions } from './createCoverQuestions';
 import { createHeader } from './createHeader';
-import { readPersonalization } from './readPersonalization';
-import { TextResponses } from './types';
+import { checkIfShouldUpdate } from './checkIfShouldUpdate';
+import { hasFilledPersonalData } from './hasFilledPersonalData';
+import { readDefaultPersonalization, readPersonalization } from './readPersonalization';
+import { PersonalData, TextResponses } from './types';
 import { writeFiles } from './writeDocs/writeFiles';
+import { createPersonalizedDataQuestions } from './updateDataQuestions';
+import { applyDefaultPersonalizationData } from './applyDefaultPersonalData';
 
 const initInquirer = async () => {
   try {
     await createHeader();
-
+    let finalData: PersonalData;
     const personalData = await readPersonalization();
     if (!personalData) {
       throw new Error('No personal data found');
     }
-
-    const coverQuestions = createCoverQuestions(personalData);
+    const hasPersonalData = hasFilledPersonalData(personalData);
+    const { shouldUpdate } = await inquirer.prompt(checkIfShouldUpdate(hasPersonalData));
+    if (shouldUpdate) {
+      console.log('Please fill out the following questions to update your personal data');
+      await createPersonalizedDataQuestions(personalData);
+    }
+    if (!hasPersonalData) {
+      const defaultPersonalizationData = await readDefaultPersonalization();
+      finalData = applyDefaultPersonalizationData(personalData, defaultPersonalizationData!);
+    } else {
+      finalData = personalData;
+    }
+    const coverQuestions = createCoverQuestions(finalData);
 
     const { outputTypes, createCopy, ...textResponses } = await inquirer.prompt(coverQuestions);
     const result: TextResponses = textResponses;
