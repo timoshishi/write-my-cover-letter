ce4e6d7f7c50ebdf7fc506425b14104662960bd8
timoshishi <timfrrst@gmail.com>
2022-11-03 17:35:39 -0400

    chore: update copy

--- src/generateParagraphs.ts
+++ src/generateParagraphs.ts
@@ -27,8 +27,8 @@ export const generateParagraphs = ({
     toWhomItMayConcern: `Dear ${company} team,`,
     introPara: `${intro} ${industryString} ${expertise}`,
     roleStr: roles[role],
-    personalIntro: `${personalIntro} ${company} promotes a culture of collaboration, diversity and commitment to quality which is both why I'm happy to be considered and am confident I would fit right in.`,
-    closer: `I'm someone who cares about people, be it product users or colleagues, and I strive to demonstrate that in everything I do. I am interested in joining your team because, as ${company} does, I value collaboration, learning and bettering the world with technology. I am looking forward to learning more about ${company}'s vision and discussing how my skill set can help your team achieve its goals. Thank you for your time and consideration.`,
+    personalIntro: `${personalIntro} ${company} promotes a culture of collaboration and commitment to quality, which is why I'm happy to be considered and am confident I would fit right in.`,
+    closer: `I am looking forward to learning more about ${company}'s vision and discussing how my skill set can help your team achieve its goals. Thank you for your time and consideration.`,
     contactInfo: [phone, email, ...sites].join(' | '),
   };
 };
